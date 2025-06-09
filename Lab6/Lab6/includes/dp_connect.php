0a1afc99718369fcd9cbd3e01f7e80d6d1cd3686
Romaro <Romaro@ROMARO-PC>
2025-06-09 19:15:30 +0300

    Final verified

--- Lab6/Lab6/includes/dp_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "admin12345", "lab6");
-if ($conn->connect_error) {
-    die("Помилка підключення: " . $conn->connect_error);
-}
-$conn->set_charset("utf8");
-?>
